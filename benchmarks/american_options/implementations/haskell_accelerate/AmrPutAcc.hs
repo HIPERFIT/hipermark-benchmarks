21b4f0e8ff18d7cab949204c45faa5bf677972ba
Troels Henriksen <athas@sigkill.dk>
2015-11-05 13:54:21 +0100

    I should do this on a machine where I can compile it.

--- benchmarks/american_options/implementations/haskell_accelerate/AmrPutAcc.hs
+++ benchmarks/american_options/implementations/haskell_accelerate/AmrPutAcc.hs
@@ -101,7 +101,7 @@ time x = do
 main :: IO ()
 main = do
   n <- liftM read getContents
-  (v, runtime) <- time $ arun ACUDA.run $ binom n
+  (v, runtime) <- time $ arun ACUDA.run $ binom $ A.constant n
   result <- getEnv "HIPERMARK_RESULT"
   writeFile result $ show v
   runtime_file <- getEnv "HIPERMARK_RUNTIME"
